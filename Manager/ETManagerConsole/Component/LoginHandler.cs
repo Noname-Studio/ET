00db7e68901cf0c2f548ab5ca8dd11e781760479
吃了会中毒 <709806504@qq.com>
2021-04-10 12:04:23 +0800

    更新新的导出插件

---
+++ Manager/ETManagerConsole/Component/LoginHandler.cs
@@ -0,0 +1,13 @@
+﻿using ET;
+
+namespace ETManagerConsole.Component
+{
+    public class LoginHandler : AMHandler<R2C_Login>
+    {
+        protected override async ETVoid Run(Session session, R2C_Login message)
+        {
+            Log.Error("!!!!");
+            await ETTask.CompletedTask;
+        }
+    }
+}
---
+++ Server/Hotfix/Handler/C2G_ReloadHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ET
+{
+    public class C2G_ReloadHandler : AMRpcHandler<C2G_Reload,G2C_Reload>
+    {
+        protected override async ETTask Run(Session session, C2G_Reload request, G2C_Reload response, Action reply)
+        {
+            Game.EventSystem.Add(DllHelper.GetHotfixAssembly());
+            reply();
+            Log.Debug("完成热更新");
+            await ETTask.CompletedTask;
+        }
+    }
+}
