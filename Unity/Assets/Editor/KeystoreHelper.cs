b82da8cb5c34b785dd443409fd320e51ec8c31ec
吃了会中毒 <709806504@qq.com>
2021-04-16 16:43:44 +0800

    提交

--- Unity/Assets/Editor/KeystoreHelper.cs
+++ Unity/Assets/Editor/KeystoreHelper.cs
@@ -1,10 +1,14 @@
+using System.IO;
 using UnityEditor;
- 
+using UnityEngine;
+
 [InitializeOnLoad]
 public class KeystoreHelper
 {
     static KeystoreHelper()
     {
+        var dir = new DirectoryInfo(Application.dataPath);
+        PlayerSettings.Android.keystoreName = dir.Parent.Parent.FullName + "/user.keystore"; 
         PlayerSettings.Android.keystorePass = "159357";
         PlayerSettings.Android.keyaliasName = "testkeystore";
         PlayerSettings.Android.keyaliasPass = "159357";
